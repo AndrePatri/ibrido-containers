ee79733c95e55635f8e060e97e09ecc44ed5b9cc
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-04-08 15:52:53 +0200

    fixed missing prefix for crizz cudagl or nvidia cudagl base images

--- cudagl2204/build_docker.sh
+++ cudagl2204/build_docker.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+# Navigate to the directory containing the Dockerfile
 cd "$(dirname "$0")"
-docker build --tag crizzard/lr-gym:2204-cudagl-basic .
+
+# Check if an argument is provided for the tag
+if [ -z "$1" ]; then
+    # If no tag is provided, use a default tag
+    TAG="mycudagldocker/mycudagl:2204-cudagl-basic"
+else
+    # If a tag is provided, use it
+    TAG="$1"
+fi
+
+# Build the Docker image with the specified tag
+docker build --tag "$TAG" .
