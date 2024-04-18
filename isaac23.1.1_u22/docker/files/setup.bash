294ddd20c73232227d37b6c52bdf31ecb95a1edc
AndrePatri <andreapatrizi1b6e6@gmail.com>
2024-04-18 15:07:01 +0200

    added setup file for workspace

---
+++ isaac23.1.1_u22/docker/files/setup.bash
@@ -0,0 +1,10 @@
+# setup environment
+export LD_LIBRARY_PATH=/root/RL_ws/hhcm/install/lib:$LD_LIBRARY_PATH
+export CMAKE_PREFIX_PATH=/root/RL_ws/hhcm/install:$CMAKE_PREFIX_PATH
+export PATH=/root/RL_ws/hhcm/install/bin:$PATH
+export GAZEBO_PLUGIN_PATH=/root/RL_ws/hhcm/install/lib:$GAZEBO_PLUGIN_PATH
+export PYTHONPATH=/root/RL_ws/hhcm/install/lib/python2.7/dist-packages:/root/RL_ws/hhcm/install/lib/python3.10/site-packages:/root/RL_ws/hhcm/install/lib/python3/dist-packages:$PYTHONPATH
+export ROS_PACKAGE_PATH=/root/RL_ws/hhcm/ros_src:/root/RL_ws/hhcm/install/share:/root/RL_ws/hhcm/install/lib:$ROS_PACKAGE_PATH
+export AMENT_PREFIX_PATH=/root/RL_ws/hhcm/install:$AMENT_PREFIX_PATH
+export PKG_CONFIG_PATH=/root/RL_ws/hhcm/install/lib/pkgconfig:$PKG_CONFIG_PATH
+export HHCM_FOREST_PATH=/root/RL_ws/hhcm:$HHCM_FOREST_PATH
