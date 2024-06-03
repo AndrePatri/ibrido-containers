c9bfddc473caa3e9c13d76cbd9d1326a759066a9
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-06-03 11:42:56 +0200

    moved to isaac4.0.0

--- isaac_ubuntu22/docker/build_docker.sh
+++ isaac_ubuntu22/docker/build_docker.sh
@@ -3,7 +3,8 @@
 # Navigate to the directory containing the Dockerfile
 cd "$(dirname "$0")"
 
-TAG="ibrido:isaac2023.1.1"
+# TAG="ibrido:isaac2023.1.1"
+TAG="ibrido:isaac4.0.0"
 
 # Build the Docker image with the specified tag
 docker build --tag "$TAG" .
--- isaac_ubuntu22/docker/launch_persistent_container.sh
+++ isaac_ubuntu22/docker/launch_persistent_container.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-IMAGE_NAME="ibrido:isaac2023.1.1"
-CONTAINER_NAME="ibrido_isaac2023.1.1"
+# IMAGE_NAME="ibrido:isaac2023.1.1"
+# CONTAINER_NAME="ibrido_isaac2023.1.1"
+IMAGE_NAME="ibrido:isaac4.0.0"
+CONTAINER_NAME="ibrido_isaac4.0.0"
 
 # Check if the container exists
 docker container inspect "$CONTAINER_NAME" > /dev/null 2>&1
@@ -15,7 +17,7 @@ if [ $? -ne 0 ]; then # if the previous command failed
         -v /etc/localtime:/etc/localtime:ro \
         --shm-size=1gb\
         --entrypoint bash \
-        -v ~/docker/ibrido-docker/isaac-sim/cache/kit:/root/.local/share/ov/pkg/isaac_sim-2023.1.1/kit/cache:rw \
+        -v ~/docker/ibrido-docker/isaac-sim/cache/kit:/isaac-sim/kit/cache:rw \
         -v ~/docker/ibrido-docker/isaac-sim/cache/ov:/root/.cache/ov:rw \
         -v ~/docker/ibrido-docker/isaac-sim/cache/pip:/root/.cache/pip:rw \
         -v ~/docker/ibrido-docker/isaac-sim/cache/glcache:/root/.cache/nvidia/GLCache:rw \
