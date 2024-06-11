4ec58ed667c05c5be58ff141dd83546b96dd2025
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-06-11 23:45:26 +0200

    fixed micromamba executable overwritten by conda mount

--- isaac_ubuntu22/singularity/build_singularity_isaac.sh
+++ isaac_ubuntu22/singularity/build_singularity_isaac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo singularity registry login --username \$oauthtoken docker://nvcr.io
-sudo singularity build ./ibrido_isaac.sif ./u22_isaac
+sudo singularity build ./ibrido_isaac.sif ./u22_isaac.def
 
--- isaac_ubuntu22/singularity/run_singularity.sh
+++ isaac_ubuntu22/singularity/run_singularity.sh
@@ -3,9 +3,7 @@
 # get script directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-source $SCRIPT_DIR/files/.env.base
-
-sudo singularity shell \
+singularity shell \
     -B /tmp/.X11-unix:/tmp/.X11-unix \
     -B /etc/localtime:/etc/localtime:ro \
     -B ~/docker/ibrido-docker/isaac-sim/cache/kit:/isaac-sim/kit/cache:rw \
@@ -20,12 +18,22 @@ sudo singularity shell \
     -B ~/docker/ibrido-docker/training_data:/root/training_data:rw \
     -B ~/docker/ibrido-docker/aux_data:/root/aux_data:rw \
     -B ~/docker/ibrido-docker/conda:/opt/conda:rw \
-    --oci --nv ibrido_isaac.oci.sif
+    --no-home \
+    --nv ibrido_isaac.sif
 
-# singularity run --oci ./ibrido.oci.sif
-# singularity shell \
+# singularity run \
+#     -B /tmp/.X11-unix:/tmp/.X11-unix \
+#     -B /etc/localtime:/etc/localtime:ro \
+#     -B ~/docker/ibrido-docker/isaac-sim/cache/kit:/isaac-sim/kit/cache:rw \
+#     -B ~/docker/ibrido-docker/isaac-sim/cache/ov:/root/.cache/ov:rw \
+#     -B ~/docker/ibrido-docker/isaac-sim/cache/pip:/root/.cache/pip:rw \
+#     -B ~/docker/ibrido-docker/isaac-sim/cache/glcache:/root/.cache/nvidia/GLCache:rw \
+#     -B ~/docker/ibrido-docker/isaac-sim/cache/computecache:/root/.nv/ComputeCache:rw \
+#     -B ~/docker/ibrido-docker/isaac-sim/logs:/root/.nvidia-omniverse/logs:rw \
+#     -B ~/docker/ibrido-docker/isaac-sim/data:/root/.local/share/ov/data:rw \
+#     -B ~/docker/ibrido-docker/isaac-sim/documents:/root/Documents:rw \
 #     -B ~/docker/ibrido-docker/ibrido_ws:/root/ibrido_ws:rw \
 #     -B ~/docker/ibrido-docker/training_data:/root/training_data:rw \
 #     -B ~/docker/ibrido-docker/aux_data:/root/aux_data:rw \
 #     -B ~/docker/ibrido-docker/conda:/opt/conda:rw \
-#     --oci --nv ibrido.oci.sif
+#     --nv ibrido_isaac.sif
