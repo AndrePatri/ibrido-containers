58461cf24696184faf94eacc22c3340f7939f6b8
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-06-11 13:47:04 +0200

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- isaac_ubuntu22/singularity/build_singularity_isaac.sh
+++ isaac_ubuntu22/singularity/build_singularity_isaac.sh
@@ -2,4 +2,4 @@
 
 #singularity registry login --username \$oauthtoken docker://nvcr.io
 # build singularity container based on Dockerfile
-singularity build --oci ./ibrido_isaac4.oci.sif ./DockerfileIsaac4
+singularity build --oci ./ibrido_isaac.oci.sif ./DockerfileIsaac4
