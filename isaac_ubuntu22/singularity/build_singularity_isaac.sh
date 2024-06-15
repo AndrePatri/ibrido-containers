dc8ff572f76031b44e72805e94a795664532d3ab
AndrePatri <andreapatrizi1b6e6@gmail.com>
2024-06-15 15:17:47 +0200

    now building container with fakerot

--- isaac_ubuntu22/singularity/build_singularity_isaac.sh
+++ isaac_ubuntu22/singularity/build_singularity_isaac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Insert your generated NVIDIA NGC password/token to pull IsaacSim-->'
-sudo singularity registry login --username \$oauthtoken docker://nvcr.io
+singularity registry login --username \$oauthtoken docker://nvcr.io # run with sudo if singularity build --fakeroot
 echo 'Starting build of IBRIDO singularity container-->'
-sudo singularity build ./ibrido_isaac.sif ./u22_isaac.def
+singularity build --fakeroot ./ibrido_isaac.sif ./u22_isaac.def # either --fakeroot or sudo are necessary
 
