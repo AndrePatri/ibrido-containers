0f8952a412ee32d97ebabddc76627b3604c6833d
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-06-16 15:49:12 +0200

    centralized bind dir, git dir creation

--- isaac_ubuntu22/singularity/build_singularity_isaac.sh
+++ isaac_ubuntu22/singularity/build_singularity_isaac.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-echo 'Insert your generated NVIDIA NGC password/token to pull IsaacSim-->'
-singularity registry login --username \$oauthtoken docker://nvcr.io # run with sudo if singularity build --fakeroot
-echo 'Starting build of IBRIDO singularity container-->'
-sudo singularity build ./ibrido_isaac.sif ./u22_isaac.def # either --fakeroot or sudo are necessary
-# singularity build --fakeroot ./ibrido_isaac.sif ./u22_isaac.def # either --fakeroot or sudo are necessary
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# retrieve dirs to be binded
+${SCRIPT_DIR}/utils/create_ws.sh
+
+# export MY_LIST="${my_list[*]}"
+
+# echo 'Insert your generated NVIDIA NGC password/token to pull IsaacSim-->'
+# singularity registry login --username \$oauthtoken docker://nvcr.io # run with sudo if singularity build --fakeroot
+# echo 'Starting build of IBRIDO singularity container-->'
+# sudo singularity build --bind ./ibrido_isaac.sif ./u22_isaac.def # either --fakeroot or sudo are necessary
+# # singularity build --fakeroot ./ibrido_isaac.sif ./u22_isaac.def # either --fakeroot or sudo are necessary
 
 
--- isaac_ubuntu22/singularity/utils/create_ws.sh
+++ isaac_ubuntu22/singularity/utils/create_ws.sh
@@ -1,56 +1,39 @@
 #!/bin/bash
 set -e # exiting if any cmd fails
 
-IBRIDO_PREFIX=$HOME/containers/ibrido-singularity
-WS_SRC=${IBRIDO_PREFIX}/ibrido_ws/src
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+root_folder="$(dirname "$THIS_DIR")"
+
+source "${root_folder}/files/bind_list.sh"
 
 echo 'Creating all directories...'
+mkdir -p $IBRIDO_WS_SRC # to hold git repos
+for item in "${IBRIDO_BDIRS_SRC[@]}"; do
+    echo "--> $item"
+    mkdir -p $item
+done
 
-mkdir -p $WS_SRC
-
-# auxiliary dirs to be mounted/binded
-mkdir -p ${IBRIDO_PREFIX}/training_data
-mkdir -p ${IBRIDO_PREFIX}/aux_data
-mkdir -p ${IBRIDO_PREFIX}/conda
-mkdir -p ${IBRIDO_PREFIX}/conda_hidden/.conda
-mkdir -p ${IBRIDO_PREFIX}/.cache/wandb
-mkdir -p ${IBRIDO_PREFIX}/network/
-touch ${IBRIDO_PREFIX}/network/.netrc
-mkdir -p ${IBRIDO_PREFIX}/.byobu
-
-# isaac-sim specific
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/cache/ov
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/cache/pip
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/cache/warp
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/cache/kit
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/cache/glcache
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/cache/computecache
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/cache/extcache
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/cache/nv_shadercache
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/logs
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/kitlogs/Isaac-Sim
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/exts
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/data
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/apps
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/documents
-mkdir -p ${IBRIDO_PREFIX}/isaac-sim/local
-
-echo 'Done.'
-
-cd $WS_SRC
-echo 'Cloning all repos into workspace (if not already there)...'
-git clone -q -b main git@github.com:AndrePatri/IBRIDO.git &
-git clone -q -b isaac4.0.0 git@github.com:AndrePatri/LRHControl.git &
-git clone -q -b devel git@github.com:AndrePatri/CoClusterBridge.git &
-git clone -q -b devel git@github.com:AndrePatri/SharsorIPCpp.git &
-git clone -q -b isaac4.0.0  git@github.com:AndrePatri/OmniRoboGym.git &
-git clone -q -b ros2_humble git@github.com:AndrePatri/RHCViz.git &
-git clone -q -b new_architecture git@github.com:AndrePatri/phase_manager.git &
-git clone -q -b andrepatri_dev git@github.com:AndrePatri/unitree_ros.git &
-git clone -q -b andrepatri_devel git@github.com:ADVRHumanoids/horizon.git &
-git clone -q -b isaac4.0.0 git@github.com:ADVRHumanoids/KyonRLStepping.git &
-git clone -q -b isaac4.0.0 git@github.com:ADVRHumanoids/CentauroHybridMPC.git &
-git clone -q -b big_wheels_v2.10_optional_find_ros2 git@github.com:ADVRHumanoids/iit-centauro-ros-pkg.git &
-git clone -q -b optional_find_ros2 git@github.com:ADVRHumanoids/iit-kyon-ros-pkg.git &
-wait
-echo 'Done.'
+echo 'Cloning repos...'
+cd $IBRIDO_WS_SRC
+for ((i = 0; i < ${#IBRIDO_GITDIRS[@]}; i++)); do
+    src="${IBRIDO_GIT_SRC[$i]}"
+    branch="${IBRIDO_GIT_BRCH[$i]}"
+    echo "--> $src # $branch"
+    git clone -q -b $branch $src
+done
+# echo 'Cloning all repos into workspace (if not already there)...'
+# git clone -q -b main git@github.com:AndrePatri/IBRIDO.git &
+# git clone -q -b isaac4.0.0 git@github.com:AndrePatri/LRHControl.git &
+# git clone -q -b devel git@github.com:AndrePatri/CoClusterBridge.git &
+# git clone -q -b devel git@github.com:AndrePatri/SharsorIPCpp.git &
+# git clone -q -b isaac4.0.0  git@github.com:AndrePatri/OmniRoboGym.git &
+# git clone -q -b ros2_humble git@github.com:AndrePatri/RHCViz.git &
+# git clone -q -b new_architecture git@github.com:AndrePatri/phase_manager.git &
+# git clone -q -b andrepatri_dev git@github.com:AndrePatri/unitree_ros.git &
+# git clone -q -b andrepatri_devel git@github.com:ADVRHumanoids/horizon.git &
+# git clone -q -b isaac4.0.0 git@github.com:ADVRHumanoids/KyonRLStepping.git &
+# git clone -q -b isaac4.0.0 git@github.com:ADVRHumanoids/CentauroHybridMPC.git &
+# git clone -q -b big_wheels_v2.10_optional_find_ros2 git@github.com:ADVRHumanoids/iit-centauro-ros-pkg.git &
+# git clone -q -b optional_find_ros2 git@github.com:ADVRHumanoids/iit-kyon-ros-pkg.git &
+# wait
+# echo 'Done.'
