a68d5e254164b98fcae0aaddb3fb15bb14526ea1
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-06-16 17:34:03 +0200

    added wandb key as buld argument

--- isaac_ubuntu22/singularity/utils/setup_ws.sh
+++ isaac_ubuntu22/singularity/utils/setup_ws.sh
@@ -61,7 +61,7 @@ pip install --no-deps -e horizon
 # copying script to launch byobu
 #cp $WS_BASEDIR/src/LRHControl/lrhc_control/scripts/launch_byobu_ws.sh /root/
 
-wandb login --relogin # login to wandb
+wandb login --relogin $WANDB_KEY # login to wandb
 
 source /root/.bashrc
 
