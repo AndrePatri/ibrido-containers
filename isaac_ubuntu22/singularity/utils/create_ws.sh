5f931bbee0f79a549920d1ef2528e4f7527d939b
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-06-16 15:51:03 +0200

    cloining with separate processes to speed up

--- isaac_ubuntu22/singularity/utils/create_ws.sh
+++ isaac_ubuntu22/singularity/utils/create_ws.sh
@@ -12,6 +12,7 @@ for item in "${IBRIDO_BDIRS_SRC[@]}"; do
     echo "--> $item"
     mkdir -p $item
 done
+echo 'Done.'
 
 echo 'Cloning repos...'
 cd $IBRIDO_WS_SRC
@@ -19,21 +20,7 @@ for ((i = 0; i < ${#IBRIDO_GITDIRS[@]}; i++)); do
     src="${IBRIDO_GIT_SRC[$i]}"
     branch="${IBRIDO_GIT_BRCH[$i]}"
     echo "--> $src # $branch"
-    git clone -q -b $branch $src
+    git clone -q -b $branch $src &
 done
-# echo 'Cloning all repos into workspace (if not already there)...'
-# git clone -q -b main git@github.com:AndrePatri/IBRIDO.git &
-# git clone -q -b isaac4.0.0 git@github.com:AndrePatri/LRHControl.git &
-# git clone -q -b devel git@github.com:AndrePatri/CoClusterBridge.git &
-# git clone -q -b devel git@github.com:AndrePatri/SharsorIPCpp.git &
-# git clone -q -b isaac4.0.0  git@github.com:AndrePatri/OmniRoboGym.git &
-# git clone -q -b ros2_humble git@github.com:AndrePatri/RHCViz.git &
-# git clone -q -b new_architecture git@github.com:AndrePatri/phase_manager.git &
-# git clone -q -b andrepatri_dev git@github.com:AndrePatri/unitree_ros.git &
-# git clone -q -b andrepatri_devel git@github.com:ADVRHumanoids/horizon.git &
-# git clone -q -b isaac4.0.0 git@github.com:ADVRHumanoids/KyonRLStepping.git &
-# git clone -q -b isaac4.0.0 git@github.com:ADVRHumanoids/CentauroHybridMPC.git &
-# git clone -q -b big_wheels_v2.10_optional_find_ros2 git@github.com:ADVRHumanoids/iit-centauro-ros-pkg.git &
-# git clone -q -b optional_find_ros2 git@github.com:ADVRHumanoids/iit-kyon-ros-pkg.git &
-# wait
-# echo 'Done.'
+wait
+echo 'Done.'
