1e6afcd0779e7b7cf3e1f21c99debf221d538358
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-06-17 12:06:38 +0200

    removed wandb key at build time for vulnerability reasons

--- isaac_ubuntu22/singularity/build_singularity_isaac.sh
+++ isaac_ubuntu22/singularity/build_singularity_isaac.sh
@@ -4,27 +4,17 @@ source "${SCRIPT_DIR}/files/bind_list.sh"
 
 # Function to print usage
 usage() {
-    echo "Usage: $0 [--use_sudo|-s] [--init|-i] [--wandb_key|-w <key>]"
+    echo "Usage: $0 [--use_sudo|-s] [--init|-i]"
     exit 1
 }
 use_sudo=false # whether to use superuser privileges
 init=false # whether to initialize/create the workspace
-wandb_key=""
 
 # Parse command line options
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -s|--use_sudo) use_sudo=true ;;
         -i|--init) init=true ;;
-        -w|--wandb_key) 
-            if [[ -n "$2" && "$2" != "-"* ]]; then
-                wandb_key=$2
-                shift
-            else
-                echo "Error: --wandb_key requires a non-empty argument."
-                usage
-            fi
-            ;;
         -h|--help) usage ;;
         *) echo "Unknown parameter passed: $1"; usage ;;
     esac
@@ -39,11 +29,11 @@ echo '--> Insert your generated NVIDIA NGC password/token to pull IsaacSim (see 
 if $use_sudo; then
     sudo singularity registry login --username \$oauthtoken docker://nvcr.io # run with sudo if singularity build --fakeroot
     echo '--> Starting building of IBRIDO singularity container (sudo)...'
-    sudo singularity build --build-arg wandb_key=${wandb_key} ./ibrido_isaac.sif ./u22_isaac.def # either --fakeroot or sudo are necessary
+    sudo singularity build ./ibrido_isaac.sif ./u22_isaac.def # either --fakeroot or sudo are necessary
 else
     singularity registry login --username \$oauthtoken docker://nvcr.io # run with sudo if singularity build --fakeroot
     echo '--> Starting building of IBRIDO singularity container (fakeroot)...'
-    singularity build --fakeroot --build-arg wandb_key=${wandb_key} ./ibrido_isaac.sif ./u22_isaac.def # either --fakeroot or sudo are necessary
+    singularity build --fakeroot ./ibrido_isaac.sif ./u22_isaac.def # either --fakeroot or sudo are necessary
 
 fi
 
--- isaac_ubuntu22/singularity/run_singularity.sh
+++ isaac_ubuntu22/singularity/run_singularity.sh
@@ -4,15 +4,27 @@ source "${SCRIPT_DIR}/files/bind_list.sh"
 
 # Function to print usage
 usage() {
-    echo "Usage: $0 [--use_sudo|-s]"
+    echo "Usage: $0 [--use_sudo|-s] [--wandb_key|-w <key>]"
     exit 1
 }
 use_sudo=false # whether to use superuser privileges
+wandb_key=""
 
 # Parse command line options
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -s|--use_sudo) use_sudo=true ;;
+        -w|--wandb_key) 
+            if [[ -n "$2" && "$2" != "-"* ]]; then
+                wandb_key=$2
+                shift
+            else
+                echo "Error: --wandb_key requires a non-empty argument."
+                usage
+            fi
+            ;;
+        -h|--help) usage ;;
+        *) echo "Unknown parameter passed: $1"; usage ;;
     esac
     shift
 done
@@ -24,6 +36,7 @@ unset IFS # Reset the internal field separator
 
 if $use_sudo; then
     sudo singularity exec \
+        --env "WANDB_KEY=$wandb_key"\
         -B /tmp/.X11-unix:/tmp/.X11-unix\
         -B /etc/localtime:/etc/localtime:ro \
         --bind $binddirs\
@@ -31,6 +44,7 @@ if $use_sudo; then
         --nv ibrido_isaac.sif bash
 else
     singularity exec \
+        --env "WANDB_KEY=$wandb_key"\
         -B /tmp/.X11-unix:/tmp/.X11-unix\
         -B /etc/localtime:/etc/localtime:ro \
         --bind $binddirs\
