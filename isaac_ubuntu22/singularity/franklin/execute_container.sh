aac5cef15dbda7fdce939c3b2abb251be073d1d4
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-06-18 14:33:59 +0200

    more reasonable settings + cleaning

--- isaac_ubuntu22/singularity/franklin/execute_container.sh
+++ isaac_ubuntu22/singularity/franklin/execute_container.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=96:mpiprocs=20:ngpus=1
-#PBS -l walltime=00:30:00
+#PBS -l select=1:ncpus=64:mpiprocs=1:ngpus=1
+#PBS -l walltime=12:00:00
 #PBS -j oe
 #PBS -N ibrido_build
 #PBS -q gpu_a100
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-source $IBRIDO_CONTAINERS_PREFIX/franklin/run_cfg.sh
+export IBRIDO_CONTAINERS_PREFIX=""
+export WANDB_KEY=""
+export COMMENT=""
 
 $IBRIDO_CONTAINERS_PREFIX/execute.sh --wandb_key $WANDB_KEY --comment $COMMENT
--- isaac_ubuntu22/singularity/franklin/setup_container.sh
+++ isaac_ubuntu22/singularity/franklin/setup_container.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=20:mpiprocs=20:ngpus=1
+#PBS -l select=1:ncpus=20:mpiprocs=1:ngpus=1
 #PBS -l walltime=00:40:00
 #PBS -j oe
 #PBS -N ibrido_build
 #PBS -q gpu_a100
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-source $IBRIDO_CONTAINERS_PREFIX/franklin/setup_cfg.sh 
+export IBRIDO_CONTAINERS_PREFIX=""
 
 $IBRIDO_CONTAINERS_PREFIX/build_singularity_isaac.sh --init --do_setup
