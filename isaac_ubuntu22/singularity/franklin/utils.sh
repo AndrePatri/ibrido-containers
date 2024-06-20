82a3cecac30ec86ee52032149c8472d85697de39
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-06-20 11:22:53 +0200

    added scriplet to send signal to job

--- isaac_ubuntu22/singularity/franklin/utils.sh
+++ isaac_ubuntu22/singularity/franklin/utils.sh
@@ -35,3 +35,39 @@ function launch_bash_session() {
     ssh -t $exec_host bash
 }
 alias bash_session="launch_bash_session"
+
+function send_signal() {
+    if [ -z "$1" ] || [ -z "$2" ]; then
+        echo "Usage: send_signal <job_id> <script_pattern> [<signal>]"
+        return 1
+    fi
+
+    local job_id=$1
+    local script_pattern=$2
+    local signal=${3:-SIGINT}  # Default to SIGINT if no signal is provided
+
+    # Get the execution host of the job
+    local exec_host=$(qstat -f $job_id | grep exec_host | cut -f 7 -d " " | cut -f 1 -d "/")
+
+    if [ -z "$exec_host" ]; then
+        echo "Could not determine the execution host for job $job_id."
+        return 1
+    fi
+
+    ssh -t $exec_host "
+        # Find the PID of the script matching the pattern
+        pid=\$(pgrep -f \"python .*${script_pattern}\")
+
+        # If the PID is found, send the specified signal
+        if [ -n \"\$pid\" ]; then
+            echo 'Sending signal ${signal} to process matching pattern \"$script_pattern\" (PID: '\$pid')'
+            kill -${signal} \$pid
+        else
+            echo 'Process matching pattern \"$script_pattern\" not found'
+        fi
+
+        # Open a bash shell
+        bash
+    "
+}
+alias send_signal="send_signal"
