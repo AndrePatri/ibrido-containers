46be70aeb41736eba6ebe9d0375a643ef9167988
Andrea.Patrizi@iit.it <apatrizi@fe01.priv.franklin.lan>
2024-06-23 11:26:59 +0200

    appending unique ID to shm to avoid conflicts with leaked shm

--- isaac_ubuntu22/singularity/files/training_cfg.sh
+++ isaac_ubuntu22/singularity/files/training_cfg.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 RB_PNAME="kyon"
-SHM_NS="ibrido"
+SHM_NS="IBRIDO"
 N_ENVS=128
 RNAME="LinVelTrack"
 SEED=0
 ULIM_N=28672
 TIMEOUT_MS=60000
 CODEGEN_OVERRIDE_BDIR=""
+
+SHM_NS+="_$(date '+%Y_%m_%d__%H_%M_%S')" # appending unique string to shm namespace
