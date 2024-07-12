9cf7029d76ed622ad87ed97c56d4a485d1c2190c
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-07-12 11:02:22 +0200

    reasonable defaults for training cfg

--- ibrido_u22/singularity/files/training_cfg.sh
+++ ibrido_u22/singularity/files/training_cfg.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-export RB_PNAME=""
-export RB_PPREFNAME="\${HOME}/ibrido_ws/src/"
+export RB_PNAME="kyon"
+export RB_PPREFNAME="\${HOME}/ibrido_ws/src/iit-kyon-ros-pkg"
 export SHM_NS="IBRIDO"
-export N_ENVS=1
+export N_ENVS=128
 export RNAME="LinVelTrack"
 export SEED=0
 export ULIM_N=28672
 export TIMEOUT_MS=60000
-export CODEGEN_OVERRIDE_BDIR=""
+export CODEGEN_OVERRIDE_BDIR="\${HOME}/aux_data/KyonRHCLusterClient_kyon0/CodeGen/kyon0Rhc"
 export COMMENT="${PBS_JOBID}__"
 export SHM_NS+="_$(date '+%Y_%m_%d__%H_%M_%S')" # appending unique string to shm namespace
 export WANDB_KEY=""
