69540271dce0abf6f59beb7c0a8ba18407bc3112
AndrePatri <andreapatrizi1b6e6@gmail.com>
2024-07-14 13:38:45 +0200

    increased n cores for container setup

--- ibrido_u22/singularity/franklin/setup_container.sh
+++ ibrido_u22/singularity/franklin/setup_container.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=20:mpiprocs=1:ngpus=1
+#PBS -l select=1:ncpus=64:mpiprocs=1:ngpus=1
 #PBS -l walltime=00:40:00
 #PBS -j oe
 #PBS -N ibrido_build
 #PBS -q a100f
 
 module load go-1.19.4/apptainer-1.1.8
-
-export IBRIDO_CONTAINERS_PREFIX=""
 
 $IBRIDO_CONTAINERS_PREFIX/build_singularity_isaac.sh --init --do_setup
