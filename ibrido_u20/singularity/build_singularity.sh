0390ee1f8de0623ace878053c28267374d53d40f
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-08-06 18:09:23 +0200

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- ibrido_u20/singularity/build_singularity.sh
+++ ibrido_u20/singularity/build_singularity.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e # exiting if any cmd fails
 
+if [ -z "$IBRIDO_CONTAINERS_PREFIX" ]; then
+    echo "IBRIDO_CONTAINERS_PREFIX variable has not been seen. Please set it to \${path_to_ibrido-containers}/ibrido_22/singularity."
+    exit
+fi
+
 source "${IBRIDO_CONTAINERS_PREFIX}/files/bind_list.sh"
 
 # Function to print usage
