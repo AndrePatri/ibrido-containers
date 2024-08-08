36e2d1e7d59be71e3e4cccf7eda2645795f0e7cf
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-08-08 18:04:42 +0200

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- ibrido_u22/singularity/franklin/setup_container.sh
+++ ibrido_u22/singularity/franklin/setup_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=64:mpiprocs=1:ngpus=1
-#PBS -l walltime=00:40:00
+#PBS -l select=1:ncpus=16:mpiprocs=1:ngpus=1
+#PBS -l walltime=00:60:00
 #PBS -j oe
 #PBS -N ibrido_build
 #PBS -q a100f
