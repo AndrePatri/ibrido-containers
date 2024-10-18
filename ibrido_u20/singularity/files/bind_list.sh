13afa030941af1c3606ecf18674bddd8bb54ec3f
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-10-18 18:30:05 +0200

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- ibrido_u20/singularity/files/bind_list.sh
+++ ibrido_u20/singularity/files/bind_list.sh
@@ -64,7 +64,7 @@ IBRIDO_GITDIRS=(
     "git@github.com:ADVRHumanoids/iit-centauro-ros-pkg.git*big_wheels_v2.10_optional_find"
     "git@github.com:ADVRHumanoids/iit-kyon-ros-pkg.git*optional_find"
     "git@github.com:AndrePatri/casadi.git*optional_float"
-    "git@gitlab.com:crzz/adarl.git*master"
+    "git@gitlab.com:crzz/adarl.git*andrepatri_dev"
     "git@gitlab.com:crzz/adarl_ros.git*andrepatri_dev"
     "git@gitlab.com:crzz/jumping_leg.git*andrepatri_dev"
 )
