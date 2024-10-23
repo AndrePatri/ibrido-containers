b473381f9cd55e6397933668a042597e91985dde
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-10-23 15:38:14 +0200

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- ibrido_u20/singularity/files/bind_list.sh
+++ ibrido_u20/singularity/files/bind_list.sh
@@ -52,7 +52,7 @@ IBRIDO_GITDIRS=(
     "git@github.com:AndrePatri/CoClusterBridge.git*devel"
     "git@github.com:AndrePatri/SharsorIPCpp.git*devel"
     "git@github.com:AndrePatri/LRHControlEnvs.git*isaac4.0.0"
-    "git@github.com:AndrePatri/RHCViz.git*ros2_humble"
+    "git@github.com:AndrePatri/RHCViz.git*ros1_noetic"
     "git@github.com:AndrePatri/PerfSleep.git*main"
     "git@github.com:AndrePatri/phase_manager.git*new_architecture"
     "git@github.com:AndrePatri/unitree_ros.git*andrepatri_dev"
