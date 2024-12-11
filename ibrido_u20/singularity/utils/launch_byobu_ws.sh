9bbbd006a14468b0be089d43fe6805b9ddda07b9
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-12-11 16:51:05 +0100

    added rosbag for ros1

--- ibrido_u20/singularity/utils/launch_byobu_ws.sh
+++ ibrido_u20/singularity/utils/launch_byobu_ws.sh
@@ -230,7 +230,6 @@ clear_terminal
 
 split_h
 execute_command "cd ${WORKING_DIR}"
-# execute_command "source /opt/ros/noetic/setup.bash"
 execute_command "source /opt/ros/noetic/setup.bash"
 execute_command "source $WS_ROOT/setup.bash"
 activate_mamba_env
@@ -240,19 +239,20 @@ export ROS_IP=$ROS_IP
 clear_terminal
 prepare_command "reset && python launch_rhc2ros_bridge.py --rhc_refs_in_h_frame --ns $SHM_NS --with_agent_refs "
 
-# split_h
-# execute_command "cd ${WORKING_DIR}"
-# # execute_command "source /opt/ros/noetic/setup.bash"
-# execute_command "source /opt/ros/noetic/setup.bash"
-# execute_command "source $WS_ROOT/setup.bash"
-# activate_mamba_env
-# increase_file_limits_locally
-# export ROS_MASTER_URI=$ROS_MASTER_URI
-# export ROS_IP=$ROS_IP
-# clear_terminal
-# prepare_command "reset && python launch_periodic_bag_dump.py --ros2 --use_shared_drop_dir --ns $SHM_NS \
-# --rhc_refs_in_h_frame --bag_sdt $BAG_SDT --ros_bridge_dt $BRIDGE_DT --dump_dt_min $DUMP_DT --env_idx $ENV_IDX_BAG --srdf_path $SRDF_PATH_ROSBAG \
-# --with_agent_refs"
+split_h
+execute_command "cd ${WORKING_DIR}"
+execute_command "source /opt/ros/noetic/setup.bash"
+execute_command "source $WS_ROOT/setup.bash"
+activate_mamba_env
+increase_file_limits_locally
+export ROS_MASTER_URI=$ROS_MASTER_URI
+export ROS_IP=$ROS_IP
+clear_terminal
+prepare_command "reset && python launch_periodic_bag_dump.py --is_training --use_shared_drop_dir \
+--ns $SHM_NS --rhc_refs_in_h_frame \
+--bag_sdt $BAG_SDT --ros_bridge_dt $BRIDGE_DT --dump_dt_min $DUMP_DT --env_idx $ENV_IDX_BAG \
+--srdf_path $SRDF_PATH_ROSBAG --with_agent_refs \
+--pub_stime"
 
 # tab 1
 new_tab
