62b468e0dd3383f014246a623d96e2fc2d66be3f
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-01-01 20:04:59 +0100

    default to using weight norm + updated configs for kyon

--- ibrido_u22/singularity/files/training_cfg_kyon.sh
+++ ibrido_u22/singularity/files/training_cfg_kyon.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# export SHM_NS+="_$(date '+%Y_%m_%d__%H_%M_%S')" # appending unique string to shm namespace
 export WANDB_KEY=""
 export SHM_NS="kyon_no_wheels" # shared mem namespace used for all shared data on CPU 
 export N_ENVS=1 # number of env to run in parallel
@@ -12,10 +11,16 @@ export CRITIC_LWIDTH=512
 export CRITIC_DEPTH=4
 export OBS_NORM=1
 export OBS_RESCALING=0
+export WEIGHT_NORM=1
+export IS_CLOSED_LOOP=1
 export COMMENT='kyon no wheels, ' # any training comment
 export URDF_PATH="${HOME}/ibrido_ws/src/iit-kyon-ros-pkg/kyon_urdf/urdf/kyon.urdf.xacro" # name of the description package for the robot
 export SRDF_PATH="${HOME}/ibrido_ws/src/iit-kyon-ros-pkg/kyon_srdf/srdf/kyon.srdf.xacro" # base path where the description package for the robot are located
-export JNT_IMP_CF_PATH="${HOME}/ibrido_ws/src/KyonRLStepping/kyonrlstepping/config/jnt_imp_config.yaml" # path to yaml file for jnt imp configuration
+export JNT_IMP_CF_PATH="${HOME}/ibrido_ws/src/KyonRLStepping/kyonrlstepping/config/jnt_imp_config_open.yaml" # path to yaml file for jnt imp configuration
+if (( $IS_CLOSED_LOOP )); then
+  export JNT_IMP_CF_PATH="${HOME}/ibrido_ws/src/KyonRLStepping/kyonrlstepping/config/jnt_imp_config.yaml"
+fi
+
 export CLUSTER_CL_FNAME="kyonrlstepping.controllers.horizon_based.kyon_rhc_cluster_client" # base path where the description package for the robot are located
 export CLUSTER_DT=0.03
 export CLUSTER_DB=1
@@ -28,17 +33,19 @@ export TRAIN_ENV_FNAME="fake_pos_env_baseline"
 export TRAIN_ENV_CNAME="FakePosEnvBaseline"
 # export TRAIN_ENV_FNAME="linvel_env_with_demo"
 # export TRAIN_ENV_CNAME="LinVelEnvWithDemo"
-export DEMO_STOP_THRESH=0.3
+# export TRAIN_ENV_FNAME="variable_flights_baseline"
+# export TRAIN_ENV_CNAME="VariableFlightsBaseline"
+export DEMO_STOP_THRESH=2.0
 export BAG_SDT=90.0
 export BRIDGE_DT=0.1
 export DUMP_DT=50.0
 export ENV_IDX_BAG=0
-export ENV_IDX_BAG_DEMO=1
-export ENV_IDX_BAG_EXPL=2
+export ENV_IDX_BAG_DEMO=-1
+export ENV_IDX_BAG_EXPL=-1
 export SRDF_PATH_ROSBAG="${HOME}/aux_data/KyonRHClusterClient_${SHM_NS}/$SHM_NS.srdf" # base path where the description package for the robot are located
 export CUSTOM_ARGS_NAMES="wheels fixed_flights adaptive_is lin_a_feedback closed_partial use_flat_ground estimate_v_root" 
 export CUSTOM_ARGS_DTYPE="xacro bool bool bool bool bool bool"
-export CUSTOM_ARGS_VALS="false true true false false true false" 
+export CUSTOM_ARGS_VALS="false true true false true true false" 
 export SET_ULIM=1 
 export ULIM_N=28672 # maximum number of open file descriptors for each process (shared memory)
 export TIMEOUT_MS=10000 # timeout after which each script autokills ([ms])
