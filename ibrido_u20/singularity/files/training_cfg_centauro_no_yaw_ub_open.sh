a76a70d9701802630a5a907930585b272167cd56
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-01-10 11:07:41 +0100

    shared mem name fix

--- ibrido_u20/singularity/files/training_cfg_centauro_no_yaw_ub_open.sh
+++ ibrido_u20/singularity/files/training_cfg_centauro_no_yaw_ub_open.sh
@@ -13,7 +13,7 @@ else
 fi 
 
 # export SHM_NS+="_$(date '+%Y_%m_%d__%H_%M_%S')" # appending unique string to shm namespace 
-export SHM_NS="centauro_big_wheels_no_yaw_ub" # shared mem namespace used for all shared data on CPU 
+export SHM_NS="centauro_big_wheels_no_yaw_ub_open" # shared mem namespace used for all shared data on CPU 
 export N_ENVS=1 # number of env to run in parallel
 export RNAME="LinVelTrackBaseline" # a descriptive base name for this run
 export SEED=1 # random n generator seed to be used for this run
