dd02e80ce00e02742d5fdfc7ac7d47ef375c25e0
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-03-17 09:57:19 +0100

    added eval configs for kyon + added missing run names

--- ibrido_u20/singularity/files/training_cfgs/evals/training_cfg_centauro_no_yaw_open_eval.sh
+++ ibrido_u20/singularity/files/training_cfgs/evals/training_cfg_centauro_no_yaw_open_eval.sh
@@ -26,7 +26,7 @@ fi
 # export SHM_NS+="_$(date '+%Y_%m_%d__%H_%M_%S')" # appending unique string to shm namespace 
 export SHM_NS="centauro_big_wheels_no_yaw_open" # shared mem namespace used for all shared data on CPU 
 export N_ENVS=1 # number of env to run in parallel
-export RNAME="" # a descriptive base name for this run
+export RNAME="CentauroOpenNoYawReal" # a descriptive base name for this run
 export SEED=1 # random n generator seed to be used for this run
 export REMOTE_STEPPING=1
 export COMPRESSION_RATIO=0.6
