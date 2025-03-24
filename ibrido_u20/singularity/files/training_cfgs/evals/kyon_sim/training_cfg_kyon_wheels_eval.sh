21afb2ace79c9d0f22984ca6c1935aebd760cbea
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-03-24 19:30:20 +0100

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- ibrido_u20/singularity/files/training_cfgs/evals/kyon_sim/training_cfg_kyon_wheels_eval.sh
+++ ibrido_u20/singularity/files/training_cfgs/evals/kyon_sim/training_cfg_kyon_wheels_eval.sh
@@ -4,10 +4,10 @@ export DET_EVAL=1
 export EVAL_ON_CPU=1
 export OVERRIDE_ENV=0
 export OVERRIDE_AGENT_REFS=1
-export MPATH="/root/training_data/d2025_03_16_h15_m42_s03-KyonFullyCloopWheelsSeedAblation_FakePosEnvBaseline"
-export MNAME="d2025_03_16_h15_m42_s03-KyonFullyCloopWheelsSeedAblation_FakePosEnvBaseline_model"
+export MPATH="/root/IBRIDO/KyonPartialCloopWheelsSeedAblation_FakePosEnvBaseline/d2025_03_23_h21_m47_s57-KyonPartialCloopWheelsSeedAblation_FakePosEnvBaseline"
+export MNAME="d2025_03_23_h21_m47_s57-KyonPartialCloopWheelsSeedAblation_FakePosEnvBaseline_model"
 
-export XBOT_CONFIG="KyonRLStepping/kyonrlstepping/config/xmj_env_files/xbot2_basic.yaml"
+export XBOT_CONFIG="KyonRLStepping/kyonrlstepping/config/xmj_env_files/xbot2_basic_wheels.yaml"
 export XMJ_FILES_DIR="KyonRLStepping/kyonrlstepping/config/xmj_env_files"
 
 export RT_DEPLOY=1
@@ -17,9 +17,9 @@ export ROS_MASTER_URI="http://127.0.0.1:11311"
 export ROS_IP="127.0.0.1"
 
 # export SHM_NS+="_$(date '+%Y_%m_%d__%H_%M_%S')" # appending unique string to shm namespace 
-export SHM_NS="kyon_no_wheels" # shared mem namespace used for all shared data on CPU 
+export SHM_NS="kyon_wheels" # shared mem namespace used for all shared data on CPU 
 export N_ENVS=1 # number of env to run in parallel
-export RNAME="KyonFullyCloopWheelsReal" # a descriptive base name for this run
+export RNAME="KyonPartialCloopWheelsSeedAblation" # a descriptive base name for this run
 export SEED=1 # random n generator seed to be used for this run
 export REMOTE_STEPPING=1
 export COMPRESSION_RATIO=0.6
@@ -40,9 +40,9 @@ export DEMO_ENVS_PERC=0.0
 export EXPL_ENVS_PERC=0.0
 export ACTION_REPEAT=5
 export USE_SAC=1
-export DISCOUNT_FACTOR=0.998
+export DISCOUNT_FACTOR=0.99
 export USE_PERIOD_RESETS=0
-export COMMENT='kyon wheels fully cloop, eval on real robot' # any training comment
+export COMMENT='kyon wheels CLOSED partial, eval on real robot' # any training comment
 export URDF_PATH="${HOME}/ibrido_ws/src/iit-kyon-ros-pkg/kyon_urdf/urdf/kyon.urdf.xacro" # name of the description package for the robot
 export SRDF_PATH="${HOME}/ibrido_ws/src/iit-kyon-ros-pkg/kyon_srdf/srdf/kyon.srdf.xacro" # base path where the description package for the robot are located
 export JNT_IMP_CF_PATH="${HOME}/ibrido_ws/src/KyonRLStepping/kyonrlstepping/config/jnt_imp_config_open.yaml" # path to yaml file for jnt imp configuration
@@ -71,6 +71,6 @@ export TIMEOUT_MS=30000 # timeout after which each script autokills ([ms])
 
 export CUSTOM_ARGS_NAMES="wheels fixed_flights adaptive_is lin_a_feedback use_diff_vels xmj_timeout xmj_files_dir state_from_xbot estimate_v_root closed_partial fully_closed"
 export CUSTOM_ARGS_DTYPE="xacro bool bool bool bool int string bool bool bool bool"
-export CUSTOM_ARGS_VALS="true true false false false $TIMEOUT_MS $HOME/ibrido_ws/src/$XMJ_FILES_DIR false false false true"
+export CUSTOM_ARGS_VALS="true true true false false $TIMEOUT_MS $HOME/ibrido_ws/src/$XMJ_FILES_DIR false false true false"
 export REMOTE_ENV_FNAME="lrhcontrolenvs.envs.xmj_env"  
 
