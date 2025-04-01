54b5275a7736cb3402a9baa9c344f555b0d7d4c5
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-04-01 14:20:31 +0100

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- ibrido_u20/singularity/files/training_cfgs/evals/centauro_sim/training_cfg_centauro_eval.sh
+++ ibrido_u20/singularity/files/training_cfgs/evals/centauro_sim/training_cfg_centauro_eval.sh
@@ -24,7 +24,7 @@ else
 fi
 
 # export SHM_NS+="_$(date '+%Y_%m_%d__%H_%M_%S')" # appending unique string to shm namespace 
-export SHM_NS="centauro_big_wheels_open" # shared mem namespace used for all shared data on CPU 
+export SHM_NS="centauro_big_wheels" # shared mem namespace used for all shared data on CPU 
 export N_ENVS=1 # number of env to run in parallel
 export RNAME="CentauroCLoopPartialNoWheelsActRepAblation" # a descriptive base name for this run
 export SEED=1 # random n generator seed to be used for this run
