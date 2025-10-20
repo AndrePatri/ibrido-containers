a0462931479328cd29cff36f927aa42f9d47a6d1
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-10-20 15:24:32 +0200

    using separate xbot cfg for rt deployment

--- ibrido_u20/singularity/files/training_cfgs/examples/kyon_real/training_cfg_kyon_wheels_no_yaw.sh
+++ ibrido_u20/singularity/files/training_cfgs/examples/kyon_real/training_cfg_kyon_wheels_no_yaw.sh
@@ -7,19 +7,29 @@ export OVERRIDE_AGENT_REFS=1
 export MPATH=""
 export MNAME=""
 
-export XBOT_CONFIG="KyonRLStepping/kyonrlstepping/config/xmj_env_files/xbot2_basic_wheels.yaml"
-export XMJ_FILES_DIR="KyonRLStepping/kyonrlstepping/config/xmj_env_files"
+if [[ $RT_DEPLOY -eq 1 ]]; then
+  export XBOT_CONFIG="KyonRLStepping/kyonrlstepping/config/xmj_env_files/kyon_real/xbot2_basic_wheels_no_yaw_real.yaml"
+else
+  export XBOT_CONFIG="KyonRLStepping/kyonrlstepping/config/xmj_env_files/kyon_real/xbot2_basic_wheels_no_yaw.yaml"
+fi
+export XMJ_FILES_DIR="KyonRLStepping/kyonrlstepping/config/xmj_env_files/kyon_real"
 
-export RT_DEPLOY=1
+export RT_DEPLOY=0
 
-# Set ROS_MASTER_URI and ROS_IP for local setup
-export ROS_MASTER_URI="http://127.0.0.1:11311"
-export ROS_IP="127.0.0.1"
+if [[ $RT_DEPLOY -eq 1 ]]; then
+  # Set ROS_MASTER_URI and ROS_IP for deployment
+  export ROS_MASTER_URI="http://10.24.4.100:11311" # Centauro embedded
+  export ROS_IP=$(hostname -I | awk '{print $1}') # Extract first IP address
+else
+  # Set ROS_MASTER_URI and ROS_IP for local setup
+  export ROS_MASTER_URI="http://127.0.0.1:11311"
+  export ROS_IP="127.0.0.1"
+fi
 
 # export SHM_NS+="_$(date '+%Y_%m_%d__%H_%M_%S')" # appending unique string to shm namespace 
-export SHM_NS="kyon_real_wheels" # shared mem namespace used for all shared data on CPU 
+export SHM_NS="kyon_real_wheels_no_yaw" # shared mem namespace used for all shared data on CPU 
 export N_ENVS=1 # number of env to run in parallel
-export RNAME="KyonPartialCloopWheelsSeedAblation" # a descriptive base name for this run
+export RNAME="KyonRealCloopPartialWheelsNoYaw" # a descriptive base name for this run
 export SEED=1 # random n generator seed to be used for this run
 export REMOTE_STEPPING=0
 export COMPRESSION_RATIO=0.6
@@ -40,17 +50,17 @@ export DEMO_ENVS_PERC=0.0
 export EXPL_ENVS_PERC=0.0
 export ACTION_REPEAT=5
 export USE_SAC=1
-export DISCOUNT_FACTOR=0.99
+export DISCOUNT_FACTOR=0.998
 export USE_PERIOD_RESETS=0
-export COMMENT='kyon wheels CLOSED partial, eval on real robot' # any training comment
-export URDF_PATH="${HOME}/ibrido_ws/src/iit-kyon-ros-pkg/kyon_urdf/urdf/kyon.urdf.xacro" # name of the description package for the robot
-export SRDF_PATH="${HOME}/ibrido_ws/src/iit-kyon-ros-pkg/kyon_srdf/srdf/kyon.srdf.xacro" # base path where the description package for the robot are located
-export JNT_IMP_CF_PATH="${HOME}/ibrido_ws/src/KyonRLStepping/kyonrlstepping/config/jnt_imp_config_open.yaml" # path to yaml file for jnt imp configuration
+export COMMENT='kyon no wheels real cloop partial, ' # any training comment
+export URDF_PATH="${HOME}/ibrido_ws/src/iit-kyon-description/kyon_urdf/urdf/kyon.urdf.xacro" # name of the description package for the robot
+export SRDF_PATH="${HOME}/ibrido_ws/src/iit-kyon-description/kyon_srdf/srdf/kyon.srdf.xacro" # base path where the description package for the robot are located
+export JNT_IMP_CF_PATH="${HOME}/ibrido_ws/src/KyonRLStepping/kyonrlstepping/config/jnt_imp_config_kyon_real_no_yaw_open.yaml" # path to yaml file for jnt imp configuration
 if (( $IS_CLOSED_LOOP )); then
-  export JNT_IMP_CF_PATH="${HOME}/ibrido_ws/src/KyonRLStepping/kyonrlstepping/config/jnt_imp_config.yaml"
+  export JNT_IMP_CF_PATH="${HOME}/ibrido_ws/src/KyonRLStepping/kyonrlstepping/config/jnt_imp_config_kyon_real_no_yaw.yaml"
 fi
-export CLUSTER_CL_FNAME="kyonrlstepping.controllers.horizon_based.kyon_rhc_cluster_client" # base path where the description package for the robot are located
-export CLUSTER_DT=0.03
+export CLUSTER_CL_FNAME="kyonrlstepping.controllers.horizon_based.kyon_real_rhc_cluster_client" # base path where the description package for the robot are located
+export CLUSTER_DT=0.04
 export PHYSICS_DT=0.0005
 export N_NODES=31
 export CLUSTER_DB=1
@@ -68,10 +78,18 @@ export ENV_IDX_BAG=0
 export SRDF_PATH_ROSBAG="${HOME}/aux_data/KyonRHCLusterClient_${SHM_NS}/$SHM_NS.srdf" # base path where the description package for the robot are located
 export SET_ULIM=1 
 export ULIM_N=28672 # maximum number of open file descriptors for each process (shared memory)
-export TIMEOUT_MS=60000 # timeout after which each script autokills ([ms])
+export TIMEOUT_MS=30000 # timeout after which each script autokills ([ms])
 
-export CUSTOM_ARGS_NAMES="wheels fixed_flights adaptive_is lin_a_feedback use_diff_vels xmj_timeout xmj_files_dir state_from_xbot estimate_v_root closed_partial fully_closed"
-export CUSTOM_ARGS_DTYPE="xacro bool bool bool bool int string bool bool bool bool"
-export CUSTOM_ARGS_VALS="true true true false false $TIMEOUT_MS $HOME/ibrido_ws/src/$XMJ_FILES_DIR false false true false"
-export REMOTE_ENV_FNAME="aug_mpc_envs.envs.xmj_env"  
+if [[ $RT_DEPLOY -eq 1 ]]; then
+  export CUSTOM_ARGS_NAMES="wheels fix_yaw fixed_flights adaptive_is lin_a_feedback closed_partial fully_closed estimate_v_root base_linkname use_diff_vels xmj_timeout xmj_files_dir state_from_xbot rt_safety_perf_coeff use_mpc_pos_for_robot" 
+  export CUSTOM_ARGS_DTYPE="xacro bool bool bool bool bool bool bool str bool int string bool float bool"
+  export CUSTOM_ARGS_VALS="true true true true false true false false pelvis false $TIMEOUT_MS $HOME/ibrido_ws/src/$XMJ_FILES_DIR true 1.0 true" 
+  export REMOTE_ENV_FNAME="aug_mpc_envs.envs.xmj_env"  
+
+else
+  export CUSTOM_ARGS_NAMES="wheels fix_yaw fixed_flights adaptive_is lin_a_feedback closed_partial fully_closed estimate_v_root base_linkname use_diff_vels xmj_timeout xmj_files_dir state_from_xbot" 
+  export CUSTOM_ARGS_DTYPE="xacro bool bool bool bool bool bool bool str bool int string bool"
+  export CUSTOM_ARGS_VALS="true true true true false true false false pelvis false $TIMEOUT_MS $HOME/ibrido_ws/src/$XMJ_FILES_DIR false" 
+  export REMOTE_ENV_FNAME="aug_mpc_envs.envs.xmj_env"  
+fi
 
