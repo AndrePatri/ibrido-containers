3b8438d9d47bf11a933701d99bea63fd96cc0dfc
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-10-20 19:19:22 +0200

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- ibrido_u22/singularity/franklin/slurm/setup_container.sh
+++ ibrido_u22/singularity/franklin/slurm/setup_container.sh
@@ -9,11 +9,13 @@
 #SBATCH --output=%x.%j.out
 #SBATCH --error=%x.%j.out
 
-if [ -z "$NGC_KEY" ]; then
-    echo "NGC_KEY variable has not been seen. Please set it to your generated NVIDIA NGC password/token."
-    exit 1
-fi
+# if [ -z "$NGC_KEY" ]; then
+#     echo "NGC_KEY variable has not been seen. Please set it to your generated NVIDIA NGC password/token."
+#     exit 1
+# fi
 
 module load apptainer-1.4.1
 
+# $IBRIDO_CONTAINERS_PREFIX/build_singularity_isaac.sh --init -b --do_setup --ngc_key $NGC_KEY
 $IBRIDO_CONTAINERS_PREFIX/build_singularity_isaac.sh --init -b --do_setup --ngc_key $NGC_KEY
+
