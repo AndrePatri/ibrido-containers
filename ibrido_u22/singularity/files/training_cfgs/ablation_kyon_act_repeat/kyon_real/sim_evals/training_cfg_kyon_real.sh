03c8aab45a269421e8f3130b0680d8ee897b063b
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-10-21 12:44:15 +0200

    minor default change

--- ibrido_u22/singularity/files/training_cfgs/ablation_kyon_act_repeat/kyon_real/sim_evals/training_cfg_kyon_real.sh
+++ ibrido_u22/singularity/files/training_cfgs/ablation_kyon_act_repeat/kyon_real/sim_evals/training_cfg_kyon_real.sh
@@ -2,7 +2,7 @@
 export EVAL=1
 export DET_EVAL=1
 export EVAL_ON_CPU=1
-export OVERRIDE_ENV=0
+export OVERRIDE_ENV=1
 export OVERRIDE_AGENT_REFS=1
 export MPATH="/root/training_data/d2025_10_20_h17_m41_s15-KyonRealPartialCloopNoWheels_FakePosEnvBaseline"
 export MNAME="d2025_10_20_h17_m41_s15-KyonRealPartialCloopNoWheels_FakePosEnvBaseline_model"
