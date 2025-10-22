a5102113498dbd2e4574f0bb13532b7dbb24ae57
Andrea.Patrizi@iit.it <apatrizi@fe01.priv.franklin.lan>
2025-10-22 18:17:04 +0200

    minor slurm changes

--- ibrido_u22/singularity/franklin/slurm/execute_container.sh
+++ ibrido_u22/singularity/franklin/slurm/execute_container.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=64
 #SBATCH --gres=gpu:1
-#SBATCH --time=24:00:00
+#SBATCH --time=18:00:00
 #SBATCH --job-name=ibrido_run
 #SBATCH --partition=gpua
 #SBATCH --output=%x.%j.out
@@ -14,4 +14,4 @@ module load apptainer-1.4.1
 export SCHED_JOBID="${SLURM_JOB_ID:-$PBS_JOBID}"
 
 $IBRIDO_CONTAINERS_PREFIX/franklin/slurm/prescia_script.sh &
-$IBRIDO_CONTAINERS_PREFIX/execute.sh --cfg $cfg
+$IBRIDO_CONTAINERS_PREFIX/execute.sh --cfg "$1"
