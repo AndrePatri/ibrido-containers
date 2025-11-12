09a93c8801e06e7298382d00f0be12af1010f82f
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-11-12 17:46:00 +0100

    added new configs for centauro in cloop for both wheels and no wheels

--- ibrido_u20/singularity/files/training_cfgs/evals/centauro_sim/training_cfg_centauro_new.sh
+++ ibrido_u20/singularity/files/training_cfgs/evals/centauro_sim/training_cfg_centauro_new.sh
@@ -4,8 +4,8 @@ export DET_EVAL=1
 export EVAL_ON_CPU=1
 export OVERRIDE_ENV=1
 export OVERRIDE_AGENT_REFS=1
-export MPATH="/root/IBRIDO/CentauroCloopANoYawActRepAblation_FakePosEnvBaseline/d2025_03_21_h19_m47_s17-CentauroCloopANoYawActRepAblation_FakePosEnvBaseline"
-export MNAME="d2025_03_21_h19_m47_s17-CentauroCloopANoYawActRepAblation_FakePosEnvBaseline_model"
+export MPATH="/root/IBRIDO/CentauroCLoopPartialNoWheelsActRepAblation_FakePosEnvBaseline/d2025_03_24_h20_m58_s24-CentauroCLoopPartialNoWheelsActRepAblation_FakePosEnvBaseline"
+export MNAME="d2025_03_24_h20_m58_s24-CentauroCLoopPartialNoWheelsActRepAblation_FakePosEnvBaseline_model"
 
 export XBOT_CONFIG="CentauroHybridMPC/centaurohybridmpc/config/xmj_env_files/xbot2_basic.yaml"
 # export XBOT_CONFIG="KyonRLStepping/kyonrlstepping/config/xmj_env_files/xbot2_basic_wheels.yaml"
@@ -27,9 +27,9 @@ export ROS_MASTER_URI="http://127.0.0.1:11311"
 export ROS_IP="127.0.0.1"
 
 # export SHM_NS+="_$(date '+%Y_%m_%d__%H_%M_%S')" # appending unique string to shm namespace 
-export SHM_NS="centauro_big_wheels_no_yaw" # shared mem namespace used for all shared data on CPU 
+export SHM_NS="centauro_big_wheels" # shared mem namespace used for all shared data on CPU 
 export N_ENVS=1 # number of env to run in parallel
-export RNAME="CentauroCloopANoYawSim" # a descriptive base name for this run
+export RNAME="CentauroCLoopPartialNoWheelsActRepAblation" # a descriptive base name for this run
 export SEED=1 # random n generator seed to be used for this run
 export REMOTE_STEPPING=1
 export COMPRESSION_RATIO=0.6
@@ -52,12 +52,12 @@ export ACTION_REPEAT=5
 export USE_SAC=1
 export DISCOUNT_FACTOR=0.998
 export USE_PERIOD_RESETS=0
-export COMMENT='centauro big wheels no yaw CLOOP adaptive' # any training comment
+export COMMENT='centauro big wheels OPEN, eval on real robot' # any training comment
 export URDF_PATH="${HOME}/ibrido_ws/src/iit-centauro-ros-pkg/centauro_urdf/urdf/centauro.urdf.xacro" # name of the description package for the robot
 export SRDF_PATH="${HOME}/ibrido_ws/src/iit-centauro-ros-pkg/centauro_srdf/srdf/centauro.srdf.xacro" # base path where the description package for the robot are located
-export JNT_IMP_CF_PATH="${HOME}/ibrido_ws/src/CentauroHybridMPC/centaurohybridmpc/config/jnt_imp_config_open_with_ub.yaml" # path to yaml file for jnt imp configuration
+export JNT_IMP_CF_PATH="${HOME}/ibrido_ws/src/CentauroHybridMPC/centaurohybridmpc/config/jnt_imp_config_open.yaml" # path to yaml file for jnt imp configuration
 if (( $IS_CLOSED_LOOP )); then
-  export JNT_IMP_CF_PATH="${HOME}/ibrido_ws/src/CentauroHybridMPC/centaurohybridmpc/config/jnt_imp_config_with_ub.yaml"
+  export JNT_IMP_CF_PATH="${HOME}/ibrido_ws/src/CentauroHybridMPC/centaurohybridmpc/config/jnt_imp_config.yaml"
 fi
 export CLUSTER_CL_FNAME="centaurohybridmpc.controllers.horizon_based.centauro_rhc_cluster_client" # base path where the description package for the robot are located
 export CLUSTER_DT=0.05
@@ -81,13 +81,13 @@ export ULIM_N=28672 # maximum number of open file descriptors for each process (
 export TIMEOUT_MS=30000 # timeout after which each script autokills ([ms])
 
 if [[ $RT_DEPLOY -eq 1 ]]; then
-  export CUSTOM_ARGS_NAMES="add_remote_exit_flag step_height control_wheels fixed_flights adaptive_is lin_a_feedback closed_partial use_diff_vels state_from_xbot rt_safety_perf_coeff estimate_v_root add_upper_body use_mpc_pos_for_robot fix_yaw"
-  export CUSTOM_ARGS_DTYPE="bool float bool bool bool bool bool bool bool float bool bool bool bool"
-  export CUSTOM_ARGS_VALS="true 0.15 true true true false false false true 1.0 false false true true"
+  export CUSTOM_ARGS_NAMES="add_remote_exit_flag step_height control_wheels fixed_flights adaptive_is lin_a_feedback closed_partial use_diff_vels state_from_xbot rt_safety_perf_coeff estimate_v_root add_upper_body use_mpc_pos_for_robot torque_correction xbot2_filter_prof use_jnt_v_feedback"
+  export CUSTOM_ARGS_DTYPE="bool float bool bool bool bool bool bool bool float bool bool bool float str bool"
+  export CUSTOM_ARGS_VALS="true 0.10 false true true false true false true 1.0 false false true 1.0 fast true"
   export REMOTE_ENV_FNAME="aug_mpc_envs.envs.rt_deploy_env"
 else
-  export CUSTOM_ARGS_NAMES="step_height render_to_file render_fps control_wheels fixed_flights adaptive_is lin_a_feedback use_diff_vels xmj_timeout xmj_files_dir state_from_xbot closed_partial fix_yaw"
-  export CUSTOM_ARGS_DTYPE="float bool float bool bool bool bool bool int string bool bool bool"
-  export CUSTOM_ARGS_VALS="0.10 false 60.0 true true true false false $TIMEOUT_MS $HOME/ibrido_ws/src/$XMJ_FILES_DIR true false true"
+  export CUSTOM_ARGS_NAMES="step_height render_to_file render_fps control_wheels fixed_flights adaptive_is lin_a_feedback use_diff_vels xmj_timeout xmj_files_dir state_from_xbot closed_partial torque_correction xbot2_filter_prof use_jnt_v_feedback"
+  export CUSTOM_ARGS_DTYPE="float bool float bool bool bool bool bool int string bool bool float str bool"
+  export CUSTOM_ARGS_VALS="0.10 false 60.0 false true true false false $TIMEOUT_MS $HOME/ibrido_ws/src/$XMJ_FILES_DIR true true 1.0 fast true"
   export REMOTE_ENV_FNAME="aug_mpc_envs.envs.xmj_env"  
 fi
