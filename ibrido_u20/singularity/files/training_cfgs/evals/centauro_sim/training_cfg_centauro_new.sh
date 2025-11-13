3ff6206fd650e39363d7081c42d516caddd0564c
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-11-13 19:37:27 +0100

    updated new centauro cloop config with last model

--- ibrido_u20/singularity/files/training_cfgs/evals/centauro_sim/training_cfg_centauro_new.sh
+++ ibrido_u20/singularity/files/training_cfgs/evals/centauro_sim/training_cfg_centauro_new.sh
@@ -4,8 +4,8 @@ export DET_EVAL=1
 export EVAL_ON_CPU=1
 export OVERRIDE_ENV=1
 export OVERRIDE_AGENT_REFS=1
-export MPATH="/root/IBRIDO/CentauroCLoopPartialNoWheelsActRepAblation_FakePosEnvBaseline/d2025_03_24_h20_m58_s24-CentauroCLoopPartialNoWheelsActRepAblation_FakePosEnvBaseline"
-export MNAME="d2025_03_24_h20_m58_s24-CentauroCLoopPartialNoWheelsActRepAblation_FakePosEnvBaseline_model"
+export MPATH="/root/training_data/d2025_11_13_h09_m43_s40-CentauroOpenNoWheelsActRepAblation_FakePosEnvBaseline"
+export MNAME="d2025_11_13_h09_m43_s40-CentauroOpenNoWheelsActRepAblation_FakePosEnvBaseline_model"
 
 export XBOT_CONFIG="CentauroHybridMPC/centaurohybridmpc/config/xmj_env_files/xbot2_basic.yaml"
 # export XBOT_CONFIG="KyonRLStepping/kyonrlstepping/config/xmj_env_files/xbot2_basic_wheels.yaml"
