aa02d7577537935e46bcb7e2ab371b1e76b73498
AndrePatri <andreapatrizi97@gmail.com>
2025-11-20 12:00:43 +0100

    Signed-off-by: AndrePatri <andreapatrizi97@gmail.com>

--- ibrido_u20/singularity/files/training_cfgs/evals/centauro_real/new/training_cfg_centauro_new.sh
+++ ibrido_u20/singularity/files/training_cfgs/evals/centauro_real/new/training_cfg_centauro_new.sh
@@ -71,14 +71,8 @@ export SET_ULIM=1
 export ULIM_N=28672 # maximum number of open file descriptors for each process (shared memory)
 export TIMEOUT_MS=30000 # timeout after which each script autokills ([ms])
 
-if [[ $RT_DEPLOY -eq 1 ]]; then
-  export CUSTOM_ARGS_NAMES="add_remote_exit_flag step_height control_wheels fixed_flights adaptive_is lin_a_feedback closed_partial use_diff_vels state_from_xbot rt_safety_perf_coeff estimate_v_root add_upper_body use_mpc_pos_for_robot torque_correction xbot2_filter_prof use_jnt_v_feedback"
-  export CUSTOM_ARGS_DTYPE="bool float bool bool bool bool bool bool bool float bool bool bool float str bool"
-  export CUSTOM_ARGS_VALS="true 0.10 false true true false true false true 1.0 false false true 1.0 fast true"
-  export REMOTE_ENV_FNAME="aug_mpc_envs.envs.rt_deploy_env"
-else
-  export CUSTOM_ARGS_NAMES="step_height render_to_file render_fps control_wheels fixed_flights adaptive_is lin_a_feedback use_diff_vels xmj_timeout xmj_files_dir state_from_xbot closed_partial torque_correction xbot2_filter_prof use_jnt_v_feedback"
-  export CUSTOM_ARGS_DTYPE="float bool float bool bool bool bool bool int string bool bool float str bool"
-  export CUSTOM_ARGS_VALS="0.10 false 60.0 false true true false false $TIMEOUT_MS $HOME/ibrido_ws/src/$XMJ_FILES_DIR true true 1.0 fast true"
-  export REMOTE_ENV_FNAME="aug_mpc_envs.envs.xmj_env"  
-fi
+export CUSTOM_ARGS_NAMES="add_remote_exit_flag step_height control_wheels fixed_flights adaptive_is lin_a_feedback closed_partial use_diff_vels state_from_xbot rt_safety_perf_coeff estimate_v_root add_upper_body use_mpc_pos_for_robot torque_correction xbot2_filter_prof use_jnt_v_feedback"
+export CUSTOM_ARGS_DTYPE="bool float bool bool bool bool bool bool bool float bool bool bool float str bool"
+export CUSTOM_ARGS_VALS="true 0.10 false true true false true false true 1.0 false false true 1.0 fast true"
+export REMOTE_ENV_FNAME="aug_mpc_envs.envs.rt_deploy_env"
+
