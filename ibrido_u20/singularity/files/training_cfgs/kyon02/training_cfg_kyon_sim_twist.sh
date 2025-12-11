90fe8c1ff648eb6d32dc8053e453d2a6ef3f3301
AndrePatri <andreapatrizi97@gmail.com>
2025-12-11 20:41:55 +0100

    fixed u20 world iface import paths + added cfg for pert rejection

--- ibrido_u20/singularity/files/training_cfgs/kyon02/training_cfg_kyon_sim_twist.sh
+++ ibrido_u20/singularity/files/training_cfgs/kyon02/training_cfg_kyon_sim_twist.sh
@@ -86,11 +86,11 @@ if [[ $RT_DEPLOY -eq 1 ]]; then
   export CUSTOM_ARGS_NAMES="add_remote_exit_flag step_height wheels fixed_flights adaptive_is lin_a_feedback closed_partial fully_closed estimate_v_root use_jnt_v_feedback base_linkname use_diff_vels xmj_timeout xmj_files_dir state_from_xbot rt_safety_perf_coeff use_mpc_pos_for_robot" 
   export CUSTOM_ARGS_DTYPE="bool float xacro bool bool bool bool bool bool bool str bool int string bool float bool"
   export CUSTOM_ARGS_VALS="true 0.18 false true true false true false false true pelvis false $TIMEOUT_MS $HOME/ibrido_ws/src/$XMJ_FILES_DIR true 1.0 true" 
-  export REMOTE_ENV_FNAME="aug_mpc_envs.envs.rt_deploy_env" 
+  export REMOTE_ENV_FNAME="aug_mpc.world_interfaces.rt_deploy_env" 
 
 else
   export CUSTOM_ARGS_NAMES="step_height wheels fixed_flights adaptive_is lin_a_feedback closed_partial fully_closed estimate_v_root use_jnt_v_feedback base_linkname use_diff_vels xmj_timeout xmj_files_dir state_from_xbot rt_safety_perf_coeff use_mpc_pos_for_robot torque_correction xbot2_filter_prof" 
   export CUSTOM_ARGS_DTYPE="float xacro bool bool bool bool bool bool bool str bool int string bool float bool float str"
   export CUSTOM_ARGS_VALS="0.18 false true true false true false false true pelvis false $TIMEOUT_MS $HOME/ibrido_ws/src/$XMJ_FILES_DIR false 1.0 false 1.0 fast" 
-  export REMOTE_ENV_FNAME="aug_mpc_envs.envs.xmj_env"  
+  export REMOTE_ENV_FNAME="aug_mpc.world_interfaces.xmj_env"  
 fi
