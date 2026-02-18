72bf264998fd75f43a8ada7b52fcdb06f6c32f1e
AndrePatri <andreapatrizi97@gmail.com>
2026-02-18 19:11:47 +0100

    Signed-off-by: AndrePatri <andreapatrizi97@gmail.com>

--- ibrido_u20/singularity/files/training_cfgs/centauro/training_cfg_centauro_no_yaw_ub_new.sh
+++ ibrido_u20/singularity/files/training_cfgs/centauro/training_cfg_centauro_no_yaw_ub_new.sh
@@ -69,7 +69,8 @@ export SET_ULIM=1
 export ULIM_N=28672 # maximum number of open file descriptors for each process (shared memory)
 export TIMEOUT_MS=30000 # timeout after which each script autokills ([ms])
 
-export CUSTOM_ARGS_NAMES="add_remote_exit_flag step_height control_wheels fixed_flights adaptive_is lin_a_feedback closed_partial use_diff_vels state_from_xbot rt_safety_perf_coeff estimate_v_root add_upper_body use_mpc_pos_for_robot fix_yaw torque_correction xbot2_filter_prof use_jnt_v_feedback"
-export CUSTOM_ARGS_DTYPE="bool float bool bool bool bool bool bool bool float bool bool bool bool float str bool"
-export CUSTOM_ARGS_VALS="true 0.10 true true true false true false true 1.0 false true true true 1.0 fast true"
+export DAGANA_ROOT="${HOME}/ibrido_ws/src/iit-dagana-ros-pkg/dagana_urdf"
+export CUSTOM_ARGS_NAMES="add_remote_exit_flag step_height control_wheels fixed_flights adaptive_is lin_a_feedback closed_partial use_diff_vels state_from_xbot rt_safety_perf_coeff estimate_v_root add_upper_body use_mpc_pos_for_robot fix_yaw torque_correction xbot2_filter_prof use_jnt_v_feedback end_effector_left end_effector_right dagana_root"
+export CUSTOM_ARGS_DTYPE="bool float bool bool bool bool bool bool bool float bool bool bool bool float str bool xacro xacro xacro"
+export CUSTOM_ARGS_VALS="true 0.10 true true true false true false true 1.0 false true true true 1.0 fast true ball dagana $DAGANA_ROOT"
 export REMOTE_ENV_FNAME="aug_mpc_envs.world_interfaces.rt_deploy_world_interface"
